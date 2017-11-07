3870df2f14d9fa9ad78cc43af25e8f29e59cfe55
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-06 18:32:18 -0800

    made a lot of changes, still just messing around

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,12 +1,47 @@
-from flask import Flask, render_template
+# Just playing around with flask
 
+# Importing Flack modules, as well as my own data module
+from flask import Flask, render_template
+from data import Articles
+
+# always needed
 app = Flask(__name__)
 
+# call function Articles in data.py
+Articles = Articles()
+
+# route home screen (basically so the browser has an address to read I think
 @app.route('/')
 
+# call the html file
 def index():
-
     return render_template('home.html')
+
+# routes about
+@app.route('/about')
+
+def about():
+    return render_template('about.html')
+
+# routes articles
+@app.route('/articles')
+
+# adds a variable to the articles.html
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+# adds a variable to the address
+@app.route('/article/<string:id>/')
+
+#
+def article_id(id):
+    return render_template('article.html', id=id)
+@app.route('/testing')
+
+def testing():
+
+    phrase = "Damn Zach's awesome!"
+    return render_template('testing.html', phrase=phrase)
 
 if __name__ =='__main__':
     app.run(debug=True)
---
+++ flask_project/data.py
@@ -0,0 +1,31 @@
+def Articles():
+
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem sample text sample text sample textsample text sample text sample text  can we please not? ',
+            'author': 'zach',
+            'create_date': '11/06/2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'Lorem sample text sample text sample textsample text sample text sample text  can we please not? ',
+            'author': 'someone else',
+            'create_date': '11/06/2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article tree',
+            'body': 'Lorem sample text sample text sample textsample text sample text sample text  can we please not? ',
+            'author': 'matt',
+            'create_date': '11/06/2017'
+        }
+    ]
+    return articles
+
+
+
+
+
