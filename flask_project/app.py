be6b64df6038bc71175e68a0cde876186d234178
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-08 09:54:27 -0800

    Adding first form

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,11 +1,12 @@
 # Just playing around with flask
 
 # Importing Flack modules, as well as my own data module
-from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
 # added MySQL to macbook, and added Flask module to interact with it
 from flask_mysqldb import MySQL
-from wtforms import Form
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 # always needed
 app = Flask(__name__)
@@ -45,6 +46,25 @@ def testing():
 
     phrase = "Damn Zach's awesome!"
     return render_template('testing.html', phrase=phrase)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password'[
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do match')])
+    confirm = PasswordField('confirm password')
+
+@app.route('/register', methods=['GET, POST'])
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+
+
+    return render_template('register.html', form=form)
+
 
 if __name__ =='__main__':
     app.run(debug=True)
