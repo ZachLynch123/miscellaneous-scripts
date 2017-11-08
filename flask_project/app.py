c48242f7134303248d5b8ea056cf8009aa2c920a
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-07 19:21:44 -0800

    more imports

--- flask_project/app.py
+++ flask_project/app.py
@@ -4,7 +4,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging
 from data import Articles
 from flask_mysqldb import MySQL
-
+from wtforms
 
 # always needed
 app = Flask(__name__)
