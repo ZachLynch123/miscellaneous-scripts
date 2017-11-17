882c9546d932278f3a3413df39b657cfcb64dde5
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-16 18:35:11 -0800

    String concatination

--- treehouse.py
+++ treehouse.py
@@ -1,7 +1,6 @@
 # ugh this is kind of boring wish I was learning something I knew 
 # Woops just learned somehting new. haha Python 3 always returns floats when 
 # Dividing
+# String concatination
 
-years = 22
-days = years * 365
-weeks = days / 7
+color = ['blue', 'red', 'yellow', 'black', 'white']
