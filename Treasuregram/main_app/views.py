c364f2084569ccd33e3863a10d6c018d79b0af11
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-17 14:11:48 -0800

    Attempting to impliment class and list

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -6,18 +6,18 @@ from django.shortcuts import render
 def index(request):
     # pass dynamic data to the template from the view
     # Kind of like flask with their functions
-    name = 'Gold nugget'
-    value = 1000.00
+   # name = 'Gold nugget'
+    #value = 1000.00
     # Use a context. A dictionary that maps template variable name to Python objects
     # We can access these values using their keys ('...'):
-    context = { 'treasure_name': name,
-               'treasure_value': value }
+    #context = { 'treasure_name': name,
+      #         'treasure_value': value }
     # In order to use these variables in the template, a variable form context must be surrounded in
     # {{}} double braces. Also called mustaches. All {{variables}} are replaced with context dict using the right keys
 
 
     # Pass context as third argument in render function
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'treasures': treasures})
 
 # What if you want to pass more than 1 treasure? Create a class. (OOP basics)
 
