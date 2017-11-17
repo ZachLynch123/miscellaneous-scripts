73c8182671e17fcae7638779ed34564c00ccec86
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-17 13:15:45 -0800

    Creating dynamic variables in template

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    # pass dynamic data to the template from the view
+    # Kind of like flask with their functions
+    name = 'Gold nugget'
+    value = 1000.00
+    # Use a context. A dictionary that maps template variable name to Python objects
+    context = { 'treasure_name': name,
+               'treasure_value': value }
+
+    # Pass context as third argument in render function
+    return render(request, 'index.html', context)
