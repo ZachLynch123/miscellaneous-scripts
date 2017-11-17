e1f5675db4592fd0c28a98bdc53bb032f7a6add1
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-17 07:30:24 -0800

    Made Django app to follow along with codeschool

--- treehouse.py
+++ treehouse.py
@@ -3,4 +3,23 @@
 # Dividing
 # String concatination
 
+message = "{} dogs at the place"
+msg1 = "%s cats at the place" % 8
+message.format(8)
+print(msg1)
+name = 'Zach '
+name += 'Lynch has '
+print(name + message.format(4))
+print(name + msg1)
+
 color = ['blue', 'red', 'yellow', 'black', 'white']
+
+states = [
+    'ACTIVE',
+    ['red', 'green', 'blue'],
+    'CANCELLED',
+    'FINISHED',
+    5,
+]
+states.remove(1)
+print(states)
