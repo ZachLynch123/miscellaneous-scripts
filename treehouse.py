7cc0af1dffb24a7e92434e725a4126714d67ab6a
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-16 18:18:17 -0800

    tree

--- treehouse.py
+++ treehouse.py
@@ -1,10 +1,7 @@
-curcount = 14
-planned = 5
-release = 2
-
-del planned
-
-total = curcount + release
 # ugh this is kind of boring wish I was learning something I knew 
 # Woops just learned somehting new. haha Python 3 always returns floats when 
 # Dividing
+
+years = 22
+days = years * 365
+weeks = days / 7
