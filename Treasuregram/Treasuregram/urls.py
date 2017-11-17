ed3edb1c905bab1ee98f9b531e880d2aac32c83f
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-17 12:55:44 -0800

    working through codeschool

--- Treasuregram/Treasuregram/urls.py
+++ Treasuregram/Treasuregram/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-
+from main_app import views
+"""using urls.py from the project file to dispatch urls
+"""
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # localhost/index
+    # Instead of using urls.py in the project file to dispatch urls, each "app"
+    # of the project will dispatch their own url using their specific url.py
+    url(r'^',
+        include('main_app.url'))
 ]
---
+++ Treasuregram/main_app/urls.py
@@ -0,0 +1,8 @@
+# Creating the app's URL Dispatcher
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+
+]
