1cb5b2cb66ef9e18425b2ac55bc64aab9fedc28a
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-20 15:34:38 -0800

    Made a migrate to SQLite and now creating a class in models.py

--- playsite/music/models.py
+++ playsite/music/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=100)
+    Title = models.CharField(max_length=100)
