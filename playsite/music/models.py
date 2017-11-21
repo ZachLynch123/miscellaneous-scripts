0c1ed06bbd9a460f60fa1bac6e6f5306c151b3b9
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-20 16:16:17 -0800

    Added classes to models and added app to installed_apps in django

--- playsite/music/models.py
+++ playsite/music/models.py
@@ -6,4 +6,16 @@ from django.db import models
 
 class Album(models.Model):
     artist = models.CharField(max_length=100)
-    Title = models.CharField(max_length=100)
+    album_title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+    # on_delete models.CASCADE
+    # okay so since you have a song, and it needs to be part of an album
+    # what happens when you delete the album? It deletes all songs related
+    # to that specific album. So that there are no random floating songs without a
+    # respective album
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
--- playsite/playsite/settings.py
+++ playsite/playsite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # its called main since it was originally called main
+    'music.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
