7f90d460220668d1eb1af97a48bd2228826b4961
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-21 06:45:55 -0800

    Created Links on /music/

--- playsite/music/urls.py
+++ playsite/music/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # V default homepage (index for each individual app)
     url(r'^$', views.index, name='index'),
 
+    #  /music/albumid/
+
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
 ]
--- playsite/music/views.py
+++ playsite/music/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello world!</h1>")
+    # Simple all to database that selects all objects from Album column
+    all_albums = Album.objects.all()
+    html = ''
+    # For loop that displays album title that when clicked
+    # Will take them to the detailed page of the Album with songs, artist etc.
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+
+
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Detaisl for Album_id: " + str(album_id) + "</h2>")
