42e73f45decb1433525894e767679b5dc20317a4
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-22 18:00:10 -0800

    Attempted to emiminate hard coding URLs

--- playsite/music/urls.py
+++ playsite/music/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # V default homepage (index for each individual app)
     url(r'^$', views.index, name='index'),
 
-    #  /music/albumid/
+    #  /music/<albumid>/
 
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
