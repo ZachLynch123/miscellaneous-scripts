42b7eddaf1684d7ed03a3fdca9e03ec5f3a1e4b7
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-24 13:01:43 -0800

    Commented and cleaned up code

--- playsite/music/views.py
+++ playsite/music/views.py
@@ -1,12 +1,11 @@
-from django.http import Http404
 from .models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def index(request):
     # Simple all to database that selects all objects from Album column
     all_albums = Album.objects.all()
-    # V using django.template loader
+    #  using django.template loader
     # template = loader.get_template('music/index.html')
     context = {'all_albums': all_albums,}
     # Commenting everything out
@@ -19,11 +18,16 @@ def index(request):
     return render(request,'music/index.html', context)
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album not found")
+    # Commenting out try except in favor of http404
+    album = get_object_or_404(Album, pk=album_id)
+    #try:
+    #    album = Album.objects.get(pk=album_id)
+   # except Album.DoesNotExist:
+    #    raise Http404("Album not found")
     return render(request,'music/detail.html', {'album': album})
 
 # Connected to the database and got all albums. For each album, I looped through it and displayed it in the HTTPResponse
 # when clicked takes you to the detailed page.
+# Notice how clean this looks without all the comments
+# Theres a lot of logic going on in like 12 lines of code
+
