81691a870f8065d8e189d51e83153faa00cdcda5
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-30 18:20:11 -0800

    Made the game.py file for my community challenge. Will impliment funcion too

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
-string = 'this string'
+# This exercise helps teach control flow
+import random
+play = True
 
-print(string [::-1])
+while play == True:
+    number = random.randint(1, 10)
+    guess = int(input('please guess a number 1 through 10: '))
+    if guess == number:
+        play = False
+        keep_playing = raw_input("You're right! Thanks for playing!\n Would you like to play again? (Y/N): ")
+        if keep_playing in ['y','Y']:
+            play = True
+        elif keep_playing in ['n','N'] :
+            play = False
+        else:
+            print('Please input either y or n.')
+
+    else:
+        print('please try again\n \n')
