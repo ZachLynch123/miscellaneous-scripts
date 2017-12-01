e2942ccc6953f42a5d323079eae67d87db344897
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-01 13:34:29 -0800

    added

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
 # This exercise helps teach control flow
 import random
-play = True
 
-while play == True:
-    number = random.randint(1, 10)
-    guess = int(input('please guess a number 1 through 10: '))
-    if guess == number:
-        play = False
-        keep_playing = raw_input("You're right! Thanks for playing!\n Would you like to play again? (Y/N): ")
-        if keep_playing in ['y','Y']:
-            play = True
-        elif keep_playing in ['n','N'] :
+
+def main():
+    play = True
+    while play == True:
+        number = random.randint(1, 10)
+        guess = int(input('please guess a number 1 through 10: '))
+        if guess == number:
             play = False
-        else:
-            print('Please input either y or n.')
+            keep_playing = raw_input("You're right! Thanks for playing!\n Would you like to play again? (Y/N): ")
+            if keep_playing in ['y','Y']:
+                play = True
+            elif keep_playing in ['n','N'] :
+                play = False
 
-    else:
-        print('please try again\n \n')
+        else:
+            print('please try again\n \n')
+
+if __name__ == '__main__':
+    main()
